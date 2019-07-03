c38039a96b2f78dd206802412270c1c622c0d06a
SaroshKhawar496 <sarosh.496@gmail.com>
2019-07-03 09:33:51 +0400

    First Commit to App. Have Text Input and Button

---
+++ App.js
@@ -0,0 +1,80 @@
+/**
+* Sample React Native App
+* https://github.com/facebook/react-native
+*
+* @format
+* @flow
+*/
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    placeName: ''
+  }
+  
+  placeNameChangeHandler = (val) => {
+    this.setState({
+      placeName: val
+    });
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+      <View style={styles.inputContainer}>
+      
+        <TextInput 
+
+        placeholder="An Awesome Place"
+        value={this.state.placeName} 
+        onChangeText={this.placeNameChangeHandler}
+        style={styles.placeInput}
+        />
+        
+        <Button
+        title="Add"
+        style={styles.placeButton}
+        />
+      
+      </View>
+      
+      
+      </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 26,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    inputContainer: {
+      // flex: 1,
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center"
+    },
+    placeInput: {
+      width: "70%"
+
+    }, 
+    placeButton:{
+      width: "30%"
+    }
+  });
+  
