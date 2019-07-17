5834e1909e400d7ff166454bb5d51bea84d6e11f
SaroshKhawar496 <sarosh.496@gmail.com>
2019-07-17 21:06:51 +0500

    Created the store. Connected store to App.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 /**
  * @format
  */
+import React from 'react';
 
 import {AppRegistry} from 'react-native';
+import {Provider} from 'react-redux'; //Provider connects to store and wraps the App Component
 import App from './App';
+import configureStore from './src/store/configureStore';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+//RNRedux has to be a function that returns JSX to be used below instead of App.
+AppRegistry.registerComponent(appName, () => RNRedux);
---
+++ src/store/configureStore.js
@@ -0,0 +1,13 @@
+//Main Store for the app
+import {createStore, combineReducers} from 'redux';
+import placesReducer from './reducers/places';
+
+const rootReducer = combineReducers({
+    places: placesReducer
+});
+
+const configureStore = () => {
+    return createStore(rootReducer);
+};
+
+export default configureStore;
