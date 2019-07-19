34f42fb0888c66beb62ac8dce2321aeb6c948b97
SaroshKhawar496 <sarosh.496@gmail.com>
2019-07-19 20:06:02 +0500

    clicking on place in FindPlace list will open the PlaceDetail

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -4,10 +4,28 @@ import {connect} from 'react-redux';
 import PlaceList from '../../components/PlaceList/PlaceList';
 
 class FindPlaceScreen extends Component {
+
+    itemSelectedHandler = key => {
+        const selPlace = this.props.places.find(place => { //finding the name of the place from given key from redux store available 
+            return place.key === key;
+        })
+
+        this.props.navigator.push({
+            screen: "awesome-places.PlaceDetailScreen",
+            title: selPlace.name,
+            passProps: {// will be passed as props to the PlaceDetailScreen
+                selectedPlace: selPlace
+            }
+        });
+
+    }
     render(){
         return(
             <View>
-                <PlaceList places={this.props.places}/>
+                <PlaceList 
+                places={this.props.places}
+                onItemSelected={this.itemSelectedHandler}
+                />
             </View>
         );
     }
