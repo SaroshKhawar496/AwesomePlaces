bbd0de2e96644bbe5e06b5bfb805cd98336378e1
SaroshKhawar496 <sarosh.496@gmail.com>
2019-07-19 20:39:12 +0500

    Added a SideDrawer, not linked to the Menu button yet

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 
 import configureStore from './src/store/configureStore';
 
@@ -38,6 +39,11 @@ Navigation.registerComponent(
   ()=>PlaceDetailScreen,
   store,
   Provider
+);
+
+Navigation.registerComponent(
+  "awesome-places.SideDrawer",
+  ()=>SideDrawer
 );
 
 //Start a App
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -7,17 +7,28 @@ const startTabs = () => {
     Promise.all([
         //all method takes array of promises and waits for them to resolve 
         Icon.getImageSource("md-map", 30),
-        Icon.getImageSource("ios-share-alt", 30)
+        Icon.getImageSource("ios-share-alt", 30),
+        Icon.getImageSource("ios-menu", 30)
     ]).then(sources => {
 
         Navigation.startTabBasedApp({
             tabs: [
+                // need to register any buttons you want to show up in the tabs below!
                 {
                     //screens are registerd in App.js and can be used here then
-                    screen:"awesome-places.FindPlaceScreen",
+                    screen: "awesome-places.FindPlaceScreen",
                     label: "Find Place", //on the Tab
                     title: "Find Place", //on the Toolbar
-                    icon: sources[0]
+                    icon: sources[0],
+                    navigatorButtons: {
+                        //array of buttons where each button is javascript object
+                        leftButtons: [
+                            {
+                                icon: sources[2],
+                                title: "Menu"
+                            }
+                        ]
+                    }
         
                 },
                 {
@@ -25,10 +36,23 @@ const startTabs = () => {
                     screen:"awesome-places.SharePlaceScreen",
                     label: "Share Place", //on the Tab
                     title: "Share Place", //on the Toolbar
-                    icon: sources[1]
-        
+                    icon: sources[1],
+                    navigatorButtons: {
+                        //array of buttons where each button is javascript object
+                        leftButtons: [
+                            {
+                                icon: sources[2],
+                                title: "Menu"
+                            }
+                        ]
+                    }        
                 }
-            ]    
+            ],
+            drawer: {
+                left: {
+                    screen: "awesome-places.SideDrawer"
+                }
+            }
         });   
     })
 }
