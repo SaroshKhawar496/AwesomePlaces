13d02c3dae24412390a9e2be9c7f1c3ae47d25c3
SaroshKhawar496 <sarosh.496@gmail.com>
2019-08-04 02:12:25 +0500

    Added animation to map movement

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -16,6 +16,12 @@ class PickLocation extends Component {
     //handler to change the lat/lon touched on map
     pickLocationHandler = event => {
         const coords = event.nativeEvent.coordinate;
+        //to animate the map movement
+        this.map.animateToRegion({
+            ...this.state.focusedLocation,
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        })
         this.setState(prevState => {
             return {
                 focusedLocation: {
@@ -39,10 +45,10 @@ class PickLocation extends Component {
                 <MapView
                 //initialRegion is only for the first time load
                 initialRegion={this.state.focusedLocation}
-                //region is the updated location if you touch on the map
-                region={this.state.focusedLocation}
                 style={styles.map}
                 onPress={this.pickLocationHandler}
+                //creating a reference to MapView, for use in animation
+                ref={ref => this.map = ref}
                 >
                     {marker}
                 </MapView>
